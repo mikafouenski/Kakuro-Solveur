32af3c0dfef66d193e5ba9475c61f8511f63baa0
sylvain099 <sylvain099@hotmail.fr>
2016-03-25 11:22:31 +0100

    pk

--- src/fc_solver.c
+++ src/fc_solver.c
@@ -12,44 +12,42 @@ void rebootDomain(Variable *v){
 }
 
 
-void eraseContraintSum(Variable *v, Constraints_Sum cs){
-    
-        Constraints_Sum cs = v->sum_H;
-        int sum = 0;
-        int i = 0;
-        int nb_non_inst = 0;
-        int max; 
-        while(cs->vars[i]){
-            if(cs->vars[i]->value != -1){
-                sum += cs->vars[i]->value;
-                ++nb_non_inst;
-            }
+void eraseContraintSum(Variable *v, Constraints_Sum *cs ){
+    int sum = 0;
+    int i = 0;
+    int nb_non_inst = 0;
+    int max; 
+    while(cs->vars[i]){
+        if(cs->vars[i]->value != -1){
+            sum += cs->vars[i]->value;
+            ++nb_non_inst;
         }
-        if(nb_non_inst == 1){
-            max = sum - cs->value;
-            for (i = 0; i < sizeDomain; ++i)
-            {
-                if(i != max)
-                    v->tabdomain[i] = -1;
-            }
+    }
+    if(nb_non_inst == 1){
+        max = sum - cs->value;
+        for (i = 0; i < sizeDomain; ++i)
+        {
+            if(i != max)
+                v->tabdomain[i] = -1;
         }
-        else {
-           max = sum - cs->value;
-           if(max <= 9){
-                for (i = 0;i <= max; ++i)
-                {
-                    v->tabdomain[i] = -1;
-                }
-           }
-        } 
-    
+    }
+    else {
+     max = sum - cs->value;
+     if(max <= 9){
+        for (i = 0;i <= max; ++i)
+            v->tabdomain[i] = -1;
+        
+    }
 }
-
+} 
 
 void eraseDomain(Variable *v){
     //Contraint_h
-    
+    if(v->sum_H)
+        eraseContraintSum(v,v->sum_H);
     //Contraint_V
+    if(v->sum_V)
+        eraseContraintSum(v,v->sum_V);
     //Contraint_D
     if(v->diff){
         Constraints_Diff dc = v->diff;
@@ -73,25 +71,25 @@ void fc(Variable **v){
             while(current->indice_domaine > sizeDomain || 
                 current->tabdomainVar[current->indice_domaine] == -1){
                 ++current->indice_domaine;
-            }
-            current->value = current->tabdomainVar[current->indice_domaine];
-            
-            while(current->indice_domaine > sizeDomain){
-                current->indice_domaine = 0;
-                if(i == 0){
-                  echec();
-                }
-                current->value = -1;
-                --i;
-                current = v[i];
-            }
-            ++current->indice_domaine;
-        } while(!(testContraintDiff(current->diff,current->value) 
-                && testContraintSomme(current->sum_H) 
-                && testContraintSomme(current->sum_V)));
-        ++i;
-        current = v[i];
-    }
+        }
+        current->value = current->tabdomainVar[current->indice_domaine];
+
+        while(current->indice_domaine > sizeDomain){
+            current->indice_domaine = 0;
+            if(i == 0){
+              echec();
+          }
+          current->value = -1;
+          --i;
+          current = v[i];
+      }
+      ++current->indice_domaine;
+  } while(!(testContraintDiff(current->diff,current->value) 
+    && testContraintSomme(current->sum_H) 
+    && testContraintSomme(current->sum_V)));
+  ++i;
+  current = v[i];
+}
 }
 
 
